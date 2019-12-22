ffb963594d5c159ace65f6c985ca4c7d1ad0c608
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2019-12-22 17:29:04 +0100

    Add back tests

---
+++ AssignAll/AssignAll.Test/CodeFixTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace AssignAll.Test
+{
+    [TestClass]
+    public class CodeFixTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void EmptyInitializer_PopulatesAssignmentsForAllPublicMembers()
+        {
+            var testContent = @"
+namespace SampleConsoleApp
+{
+    internal static class Program
+    {
+        private class Foo
+        {
+            public int PropInt { get; set; }
+            public string PropString { get; set; }
+            public bool FieldBool;
+        }
+
+        private static void Main(string[] args)
+        {
+            // AssignAll enable
+            Foo foo = new Foo
+            {
+            };
+        }
+    }
+}
+";
+            var fixedContent = @"
+namespace SampleConsoleApp
+{
+    internal static class Program
+    {
+        private class Foo
+        {
+            public int PropInt { get; set; }
+            public string PropString { get; set; }
+            public bool FieldBool;
+        }
+
+        private static void Main(string[] args)
+        {
+            // AssignAll enable
+            Foo foo = new Foo
+            {
+                PropInt = ,
+                PropString = ,
+                FieldBool = 
+            };
+        }
+    }
+}
+";
+            // The code fix will produce intentional compile errors,
+            // so skip new compiler diagnostics as they will just fail
+            VerifyCSharpFix(testContent, fixedContent, allowNewCompilerDiagnostics: true);
+        }
+
+        [TestMethod]
+        public void PopulatesMissingAssignmentsAfterExistingAssignments()
+        {
+            var testContent = @"
+namespace SampleConsoleApp
+{
+    internal static class Program
+    {
+        private class Foo
+        {
+            public int PropInt { get; set; }
+            public string PropString { get; set; }
+            public bool FieldBool;
+        }
+
+        private static void Main(string[] args)
+        {
+            // AssignAll enable
+            Foo foo = new Foo
+            {
+                PropInt = 1,
+                // PropString not assigned
+                FieldBool = true
+            };
+        }
+    }
+}
+";
+            var fixedContent = @"
+namespace SampleConsoleApp
+{
+    internal static class Program
+    {
+        private class Foo
+        {
+            public int PropInt { get; set; }
+            public string PropString { get; set; }
+            public bool FieldBool;
+        }
+
+        private static void Main(string[] args)
+        {
+            // AssignAll enable
+            Foo foo = new Foo
+            {
+                PropInt = 1,
+                // PropString not assigned
+                FieldBool = true
+,
+                PropString = 
+            };
+        }
+    }
+}
+";
+            // NOTE: There seems to be added an unnecessary newline before the comma by the code fix, not sure how to fix that yet
+            // The code fix will produce intentional compile errors,
+            // so skip new compiler diagnostics as they will just fail
+            VerifyCSharpFix(testContent, fixedContent, allowNewCompilerDiagnostics: true);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AssignAllAnalyzer();
+        }
+
+        protected override Microsoft.CodeAnalysis.CodeFixes.CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new CodeFixProvider();
+        }
+
+    }
+}
